edfea9bd1445da6e2d6e87d8bde65b1efb5a62e7
yurickv <116177992+yurickv@users.noreply.github.com>
2023-06-26 00:11:06 +0000

    Deploying to gh-pages from @ yurickv/goit-react-hw-05-movies@fb99a5dee2e7635082b398d2aca87d16256876db 🚀

---
+++ static/js/853.f710218c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[853],{3853:function(t,e,r){r.d(e,{jRj:function(){return o}});var n=r(9983);function o(t){return(0,n.w_)({tag:"svg",attr:{viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:"2",strokeLinecap:"round",strokeLinejoin:"round"},child:[{tag:"circle",attr:{cx:"11",cy:"11",r:"8"}},{tag:"line",attr:{x1:"21",y1:"21",x2:"16.65",y2:"16.65"}}]})(t)}}}]);
+//# sourceMappingURL=853.f710218c.chunk.js.map
